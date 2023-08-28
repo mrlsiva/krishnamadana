d9321e50c591b2ea010217a5411a7d6959945966
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2023-08-28 21:31:52 +0530

    deleted

--- resources/views/components/public/common/announcement.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="w-full bg-[#f2cfc9] p-2 text-center">
-    <p class="mb-0 text-xs text-primary tracking-widest2">For urgent orders, please whatsapp at +91-93543-59834 | Free
-        Shipping in India
-        | Free
-        International
-        Shipping
-        above ₹35,000</p>
-</div>
--- resources/views/components/public/home/banner.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="w-full relative">
-    <img src="/images/home/banner.png" alt="">
-    <div class="absolute flex items-center justify-center inset-0 banner-text">
-        <p>Each of our <span class="color-red">garments</span> is <span class="color-blue">crafted</span> uniquely and
-            exclusively with <span class="color-yellow">hand-painted</span> motifs in-house, ensuring that all our prints
-            are <span class="color-orange">developed</span> internally.</p>
-    </div>
-</section>
