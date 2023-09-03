65051241320a4738820c95d19e85820c3338664c
csarvangit <csarvan.tech@gmail.com>
2023-09-04 01:01:48 +0530

    Product list sorting

--- app/Http/Livewire/Public/Product/ProductList.php
+++ app/Http/Livewire/Public/Product/ProductList.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace App\Http\Livewire;
 namespace App\Http\Livewire\Public\Product;
 
 use App\Models\Product;
@@ -12,18 +12,43 @@ class ProductList extends Component
     use WithPagination;
    // protected $paginationTheme = 'bootstrap';
 
+    public $sortBy;
+    public $perPage;
+
+    public function mount()
+    {
+        $this->sortBy = "default";
+        $this->perPage = 12;
+    }
+
     public function render()
     {
-        return view('livewire.public.product.product-list', [
-            'products' => Product::paginate(12),
-        ])
+        if ($this->sortBy == 'featured') {
+            $products = Product::where('featured', true)->paginate($this->perPage);
+        } else if ($this->sortBy == 'a-z') {
+            $products = Product::orderBy('name', 'asc')->paginate($this->perPage);
+        } else if ($this->sortBy == 'z-a') {
+            $products = Product::orderBy('name', 'desc')->paginate($this->perPage);        
+        } else if ($this->sortBy == 'latest') {
+            $products = Product::latest()->paginate($this->perPage);
+        } else if ($this->sortBy == 'oldest') {
+            $products = Product::oldest()->paginate($this->perPage);
+        }else if ($this->sortBy == 'low') {
+            $products = Product::orderBy('display_price', 'asc')->paginate($this->perPage);
+        } else if ($this->sortBy == 'heigh') {
+            $products = Product::orderBy('display_price', 'desc')->paginate($this->perPage);
+        } else {
+            $products = Product::paginate($this->perPage);
+        }
+
+        return view('livewire.public.product.product-list', ['products' => $products])
         ->extends('public.base')
         ->section('main');         
     }
 
-     public function paginationView()
+    public function paginationView()
     {
        return 'livewire.public.paginate.pagination';
-    } 
+    }
    
 }
--- resources/views/livewire/public/product/product-list.blade.php
+++ resources/views/livewire/public/product/product-list.blade.php
@@ -1,21 +1,48 @@
-<div class="grid grid-cols-4 gap-4 p-12">
-    @foreach ($products as $product)
-        <div class="flex flex-col text-center">
-            <a href="{{ route('home.product-details', ['slug' => $product->slug]) }}" class="product__images__link">
-                <div class="relative product__images">
-                    @foreach ($product->media->slice(0, 2) as $image)
-                        <img src="{{ $image->original_url }}"
-                            class="@if ($loop->first) front-image @else back-image @endif"
-                            alt="{{ $product->name }}">
-                    @endforeach
-                </div>
-            </a>
-            <h2 class="mt-2"><a href="{{ route('home.product-details', ['slug' => $product->slug]) }}">{{ $product->name }}</a></h2>
-            <p class="price">Rs. {{ $product->display_price }}</p>
+<div>
+    <div class="filterToolbar flex w-full border-b">
+        <div class="sort-item product pt-2 pb-2 px-4">
+            <select name="sortBy" class="chosen" wire:model="sortBy">
+                <!-- <option value="default">Sort</option>
+                <option value="heigh">Featured</option> -->
+                <option value="latest" selected="selected">Date - New To Old</option>
+                <option value="oldest">Date - Old To New </option>  
+                <option value="a-z">Name - A-Z</option>
+                <option value="z-a">Name - Z-A</option>                             
+                <option value="low">Price - Low To High</option>
+                <option value="heigh">Price - High To Low</option>
+            </select>
         </div>
-    @endforeach	
-</div>
+        <!-- <div class="sort-item product">
+            <select name="perPage" class="use-chosen" wire:model="perPage">
+                <option value="1" selected="selected">1 per page</option>
+                <option value="2">2 per page</option>
+                <option value="9">9 per page</option>
+                <option value="12">12 per page</option>
+                <option value="18">18 per page</option>
+                <option value="21">21 per page</option>
+                <option value="24">24 per page</option>
+            </select>
+        </div> -->
+    </div> 
+    <div class="grid grid-cols-4 gap-4 p-12">
+        @foreach ($products as $product)
+            <div class="flex flex-col text-center">
+                <a href="{{ route('home.product-details', ['slug' => $product->slug]) }}" class="product__images__link">
+                    <div class="relative product__images">
+                        @foreach ($product->media->slice(0, 2) as $image)
+                            <img src="{{ $image->original_url }}"
+                                class="@if ($loop->first) front-image @else back-image @endif"
+                                alt="{{ $product->name }}">
+                        @endforeach
+                    </div>
+                </a>
+                <h2 class="mt-2"><a href="{{ route('home.product-details', ['slug' => $product->slug]) }}">{{ $product->name }}</a></h2>
+                <p class="price">Rs. {{ $product->display_price }}</p>
+            </div>
+        @endforeach	
+    </div>
 
-<div class="w-full flex justify-center pb-6">
-	{{ $products->links() }}
+    <div class="w-full flex justify-center pb-6">
+        {{ $products->links() }}
+    </div>
 </div>
